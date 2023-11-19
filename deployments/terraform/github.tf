4cbeeb64b5ee06ad7ce24d571fd43edff5b39ad5
Nuno Guimaraes <nuno.guimaraes01@sap.com>
2023-11-19 18:35:26 +0000

    formating terraform code

--- deployments/terraform/github.tf
+++ deployments/terraform/github.tf
@@ -1,8 +1,8 @@
 module "github_repo" {
-  source                 = "./modules/github"
-  repo_name              = var.repo_name
-  repo_description       = var.repo_description
-  TF_VAR_github_token    = var.TF_VAR_github_token
+  source              = "./modules/github"
+  repo_name           = var.repo_name
+  repo_description    = var.repo_description
+  TF_VAR_github_token = var.TF_VAR_github_token
   # github_actions_secrets = {
   #   "secret_name_1" = "secret_value_1",
   #   "secret_name_2" = "secret_value_2",
